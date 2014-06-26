22bedd48b9836f9cc7ff1587b74649dffd32fc34
Donald Hoskins <grommish@gmail.com>
2014-06-26 15:34:14 -0400

    Update to Repo Script for Device Tree changing

--- repo.sh
+++ repo.sh
@@ -3,10 +3,26 @@
 ##
 ## Copywrite 2014 - Donald Hoskins <grommish@gmail.com>
 ## on behalf of Team Octos et al.
+##
+## This script automates device tree switching in the local_manifests
+## directory.
+##
+## Usage: jet/repo.sh <branch>
+## jet/repo.sh sam, jet/repo.sh nex, etc
+##
+## Defaults to master branch (all devices) unless specified.
+
+DEVICE_TREE=$1
+: ${DEVICE_TREE:="master"}
 
 cd ".repo/local_manifests"
-git pull origin master
+git checkout $DEVICE_TREE
+git pull origin $DEVICE_TREE
+
 cd "../.."
 RDATE=`date +"%m-%d-%y %T"`
 echo $RDATE >> Repo_ChangeLog
-repo sync | egrep '(From)|(->)' >> Repo_ChangeLog
+#repo sync 2>tmp &&  egrep '(From)|(->)' tmp >> Repo_ChangeLog
+
+repo sync
+
